35fae18b6ea80c4ba0f3a7a30720c7623468c89a
amit <amitcjcpune@gmail.com>
2020-12-17 09:38:17 +0530

    By cjc

--- GitDemo/src/com/cjc/Test.java
+++ GitDemo/src/com/cjc/Test.java
@@ -4,6 +4,12 @@ package com.cjc;
 public class Test 
 {
 
+	public void sum()
+	{
+		System.out.println("Sum===Test");
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Test");
